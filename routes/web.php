4c7bac60f1c93205db79f731eafec691c7b710d5
Wu Chen-En <ap9941002@gmail.com>
2020-10-27 21:14:01 +0800

    初步建置三個路由

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Task;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //
+});
+
+Route::delete('/task/{task}', function (Task $task) {
+    //
 });
