48c9ee9d923f09955685a29bd3d775c6d6d583ea
Wu Chen-En <ap9941002@gmail.com>
2020-10-28 21:43:22 +0800

    刪除任務：修改tasks視圖_增加刪除任務表單&按鈕

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -46,6 +46,13 @@
                 </td>
                 <td>
                     <!-- 代辦：刪除按鈕 -->
+                <td>
+                    <form action="/task/{{ $task->id }}" method="POST">
+                        {{ csrf_field() }}
+                        {{ method_field('DELETE') }}
+                        <button>刪除任務</button>
+                    </form>
+                </td>
                 </td>
             </tr>
         @endforeach
